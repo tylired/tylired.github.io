f399713751be92af837bdf6c66101d71c1394d86
Tylor <tylor.reed@us.bigkaiser.com>
2024-02-14 23:48:40 -0600

    checkDarkMode() fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -21,22 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Check for dark mode
 function checkDarkMode() {
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.querySelector('#website-logo').src = "images/directdiv-design-logo-light.svg";
-    console.log('Dark mode is enabled.');
-    } else {
+    console.log('Dark mode detected');
+  } else {
     document.querySelector('#website-logo').src = "images/directdiv-design-logo-dark.svg";
-    console.log('Dark mode is disabled.');
-    }
+    console.log('Light mode detected');
+  }
 }
-
 // Run it once the DOM is loaded
-document.addEventListener('DOMContentLoaded', function() {
-checkDarkMode();
-
+document.addEventListener('DOMContentLoaded', checkDarkMode);
 // Listen for changes in color scheme
-    if (window.matchMedia) {
-        const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-        colorSchemeQuery.addEventListener('change', checkDarkMode);
-    }
-});
+if (window.matchMedia) {
+    const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    colorSchemeQuery.addEventListener('change', checkDarkMode);
+}
