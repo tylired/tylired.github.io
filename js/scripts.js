f31a6f87ac417e98ab7317daa120eb06c6ccc9cf
Tylor <tylor.reed@us.bigkaiser.com>
2024-02-14 23:53:53 -0600

    Dark mode is cool and all, but that was too buggy. Revert.

--- js/scripts.js
+++ js/scripts.js
@@ -18,21 +18,3 @@ document.addEventListener('DOMContentLoaded', function() {
   // Listen for scroll events
   window.addEventListener('scroll', checkInView);
 });
-
-// Check for dark mode
-function checkDarkMode() {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.querySelector('#website-logo').src = "images/directdiv-design-logo-light.svg";
-    console.log('Dark mode detected');
-  } else {
-    document.querySelector('#website-logo').src = "images/directdiv-design-logo-dark.svg";
-    console.log('Light mode detected');
-  }
-}
-// Run it once the DOM is loaded
-document.addEventListener('DOMContentLoaded', checkDarkMode);
-// Listen for changes in color scheme
-if (window.matchMedia) {
-    const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    colorSchemeQuery.addEventListener('change', checkDarkMode);
-}
